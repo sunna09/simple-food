e75a8eb68aec4f4b886a3befe5f07c929a88822a
sunna09 <148879928+sunna09@users.noreply.github.com>
2023-12-16 15:55:25 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,29 @@ $(function(){
   $('.nav__link, .popular__btn').removeClass('active'); 
   $(this).addClass('active'); 
 });
+
+$(".nav__link").on("click","a", function (event) {
+  event.preventDefault();
+  var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top}, 1500);
+});
+
+$('.logo').click(function() {
+  var scrollName2 = $(this).attr('data-scroll');
+    scrollElem2 = $(scrollName2);
+    scrollTop = scrollElem2.offset().top;
+    $('html, body').animate({
+      scrollTop: scrollTop
+    }, 1500);
+});
+
+$('.nav__link').click(function() {
+  var scrollName2 = $(this).attr('data-scroll');
+    scrollElem2 = $(scrollName2);
+    scrollTop = scrollElem2.offset().top;
+    $('html, body').animate({
+      scrollTop: scrollTop
+    }, 1500);
+});
 });
